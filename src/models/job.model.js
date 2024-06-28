7da9e46e3f87484336da86c57b73e111df32b7e7
Prashant Kumar <prashant4sengar96@gmail.com>
2024-06-28 17:37:12 +0530

    job model

--- src/models/job.model.js
+++ src/models/job.model.js
@@ -1,4 +1,29 @@
-import mongoose from "mongoose";
+import mongoose, { Schema } from "mongoose";
 
+const JobSchema=new Schema({
+    owner:{
+        type:Schema.Types.ObjectId,
+        ref:"User",
+        required:true
+    },
+    title:{
+        type:String,
+        required:true
+    },
+    company:{
+        type:String,
+        required:true
+    },
+    location:{
+        type:String,
+        required:true
+    },
+    discription:{
+        type:String,
+        required:true
+    }
 
+})
+
+const Job = mongoose.model("Job", JobSchema);
 
